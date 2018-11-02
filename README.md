745ad29a7afcb36399b988ce12d4fa9c4b5bc8e0
Andrey Mukamolov <fobo66@protonmail.com>
2018-11-02 05:36:24 +0300

    Release preparation

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ MD5 implementation based on [RosettaCode](http://rosettacode.org/wiki/MD5/Implem
 
 SHA-1 implementation based on [this code](https://github.com/daveho/CloudCoder/blob/master/CloudCoderModelClasses/src/org/cloudcoder/app/shared/model/SHA1.java)
 with some modifications and conversion to Kotlin.
+
+To see this project in action, run ```./gradlew run``` or ```./gradlew.bat run```
+
+To add options for app, pass args to gradle via: ```./gradlew run --args="-key value"```. 
+You can see list of the available command line options below:
+
+ * -a, or -algorithm – hashing algorithm, either MD5 or SHA1. Defaults to MD5
+ * -f or -file – path to file with clear text to encrypt
--- src/main/java/io/fobo66/crypto/Lab3.kt
+++ src/main/java/io/fobo66/crypto/Lab3.kt
@@ -1,27 +1,58 @@
 package io.fobo66.crypto
 
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.ParseException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.xml.bind.DatatypeConverter
 
-fun main(args: Array<String>) {
-    val md5 = MD5()
-    val sha1 = SHA1()
-    val testStrings = arrayOf(
-            "",
-            "a",
-            "abc",
-            "message digest",
-            "abcdefghijklmnopqrstuvwxyz",
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-            "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
-    )
 
-    for (s in testStrings) {
-        printResults(s, md5.compute(s.toByteArray()))
-        printResults(s, sha1.compute(s.toByteArray()))
+fun main(args: Array<String>) {
+    var message = "Hello World!"
+
+    val options = Options()
+    options.addOption("f", "file", true, "Input file")
+    options.addOption("a", "algorithm", true, "Hashing algorithm, either MD5 or SHA1. Defaults to MD5")
+
+    val parser = DefaultParser()
+
+    try {
+        val cmd = parser.parse(options, args)
+
+        if (cmd.hasOption('f')) {
+            val filePath = cmd.getOptionValue("file")
+            System.out.format("Reading cleartext from file %s...%n", filePath)
+            message = loadClearTextFromFile(filePath)
+        }
+        val hash = resolveHashFunction(cmd)
+
+        printResults(message, hash.compute(message.toByteArray()))
+    } catch (e: ParseException) {
+
     }
+}
+
+fun resolveHashFunction(cmd: CommandLine): Hash {
+    if (cmd.hasOption('a')) {
+        return when (cmd.getOptionValue('a').toUpperCase()) {
+            "MD5" -> MD5()
+            "SHA1", "SHA-1" -> SHA1()
+            else -> throw IllegalArgumentException("Wrong algorithm. Must be either SHA1 or MD5")
+        }
+    }
+
+    return MD5()
 }
 
 fun printResults(clearText: String, encryptedText: ByteArray) {
     println("Message: \"$clearText\"")
     println("Hashed message: " + DatatypeConverter.printHexBinary(encryptedText))
+}
+
+@Throws(IOException::class)
+private fun loadClearTextFromFile(filePath: String): String {
+    return String(Files.readAllBytes(Paths.get(filePath)))
 }
