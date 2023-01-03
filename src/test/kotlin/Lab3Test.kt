4231dc4c10fe5c4eb3729cde6a677fb6132aa92b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-01-03 22:38:46 +0200

    Added test

---
+++ src/test/kotlin/Lab3Test.kt
@@ -0,0 +1,28 @@
+import dev.fobo66.crypto.MD5
+import dev.fobo66.crypto.SHA1
+import java.security.MessageDigest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Lab3Test {
+
+    @Test
+    fun `MD5 hash is correct`() {
+        val md5 = MD5()
+        val hash = md5.compute(CLEARTEXT.toByteArray())
+        val expectedHash = MessageDigest.getInstance("MD5").digest(CLEARTEXT.toByteArray())
+        assertEquals(expectedHash.toString(Charsets.UTF_8), hash.toString(Charsets.UTF_8))
+    }
+
+    @Test
+    fun `SHA1 hash is correct`() {
+        val sha1 = SHA1()
+        val hash = sha1.compute(CLEARTEXT.toByteArray())
+        val expectedHash = MessageDigest.getInstance("SHA1").digest(CLEARTEXT.toByteArray())
+        assertEquals(expectedHash.toString(Charsets.UTF_8), hash.toString(Charsets.UTF_8))
+    }
+
+    companion object {
+        private const val CLEARTEXT = "test"
+    }
+}
