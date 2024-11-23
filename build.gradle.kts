8bcf2f3563806321c1bd354570951533d7d43883
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-11-23 12:03:54 +0200

    Updated stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version libs.versions.kotlin
+    alias(libs.plugins.kotlin)
     application
     `jvm-test-suite`
-    id("io.gitlab.arturbosch.detekt") version "1.23.7"
+    alias(libs.plugins.detekt)
 }
 
 application {
@@ -13,13 +13,13 @@ application {
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 testing {
@@ -27,6 +27,17 @@ testing {
         val test by getting(JvmTestSuite::class) {
             useKotlinTest(libs.versions.kotlin)
         }
+    }
+}
+
+tasks {
+    withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        // Target version of the generated JVM bytecode. It is used for type resolution.
+        jvmTarget = "21"
+    }
+    withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+        // Target version of the generated JVM bytecode. It is used for type resolution.
+        jvmTarget = "21"
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,10 +20,4 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            version("kotlin", "2.0.20")
-            library("cli", "org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
-        }
-    }
 }
